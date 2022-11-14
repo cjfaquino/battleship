f7cafacec7750495e72ec0a3c32240ade9fb80de
Christian Aquino <cjfaquino@gmail.com>
2022-11-13 21:30:59 -0800

    add receiveAttack

--- src/components/__tests__/createBoard.test.js
+++ src/components/__tests__/createBoard.test.js
@@ -1,15 +1,11 @@
 const { createBoard, findCell } = require('../createBoard');
 
-let board = createBoard();
-
-beforeEach(() => {
-  board = createBoard();
-});
+const board = createBoard();
 
 describe('createBattleship()', () => {
   it('should create a 10x10 grid with no input', () => {
-    const start = [{ x: 1, y: 1, ship: null }];
-    const end = [{ x: 10, y: 10, ship: null }];
+    const start = [{ x: 1, y: 1, ship: null, missed: null }];
+    const end = [{ x: 10, y: 10, ship: null, missed: null }];
     expect(board.grid).toHaveLength(100);
     expect(board.grid).toEqual(expect.arrayContaining(start));
     expect(board.grid).toEqual(expect.arrayContaining(end));
@@ -26,10 +22,10 @@ describe('createBattleship()', () => {
   });
 
   it('should place a ship length 2 at 1,1 & 2,1', () => {
-    board.placeShip(1, 1, 2, 'X');
-    const cell = findCell(1, 1, board.grid);
-    const cell2 = findCell(2, 1, board.grid);
-    const cell3 = findCell(3, 1, board.grid);
+    board.placeShip(5, 5, 2, 'X');
+    const cell = findCell(5, 5, board.grid);
+    const cell2 = findCell(6, 5, board.grid);
+    const cell3 = findCell(7, 5, board.grid);
     expect(cell.ship).not.toBeNull();
     expect(cell2.ship).not.toBeNull();
     expect(cell3.ship).toBeNull();
@@ -40,5 +36,25 @@ describe('createBattleship()', () => {
     expect(board.placeShip(0, 1, 2, 'X')).toBe('Out of bounds');
     expect(board.placeShip(1, 11, 2, 'X')).toBe('Out of bounds');
     expect(board.placeShip(1, 0, 2, 'X')).toBe('Out of bounds');
+  });
+
+  test('a cell with a ship (1,1) should take damage from receiveAttack at (1,1)', () => {
+    const { ship } = findCell(1, 1, board.grid);
+    ship.hit();
+
+    expect(board.receiveAttack(1, 1).ship).toStrictEqual(ship);
+    expect(board.receiveAttack(1, 1).missed).toBe(false);
+  });
+
+  it('should record a missed attack', () => {
+    board.receiveAttack(3, 3);
+    const cell = findCell(3, 3, board.grid);
+
+    expect(cell.missed).toBe(true);
+  });
+
+  it('should not allow out of bounds attacks', () => {
+    expect(board.receiveAttack(11, 1)).toBe('Out of bounds');
+    expect(board.receiveAttack(1, 11)).toBe('Out of bounds');
   });
 });
--- src/components/createBoard.js
+++ src/components/createBoard.js
@@ -1,6 +1,11 @@
 const createShip = require('./createShip');
 
-const createCell = (x, y) => ({ x, y, ship: null });
+const createCell = (x, y) => ({
+  x,
+  y,
+  ship: null,
+  missed: null,
+});
 
 const findCell = (x, y, arr) =>
   arr.find((cell) => cell.x === x && cell.y === y);
@@ -32,7 +37,22 @@ const createBoard = (n = 10) => {
     return ship;
   };
 
-  return { grid, placeShip };
+  const receiveAttack = (x, y) => {
+    if (!findCell(x, y, grid)) return 'Out of bounds';
+
+    const cell = findCell(x, y, grid);
+    if (!cell.ship) {
+      cell.missed = true;
+      return cell;
+    }
+
+    const { ship } = cell;
+    cell.missed = false;
+    ship.hit();
+    return cell;
+  };
+
+  return { grid, placeShip, receiveAttack };
 };
 
 module.exports = { createBoard, findCell };
